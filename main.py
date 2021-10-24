feaa1762c55db99465452afc252d8a3f5038f422
Dennis Concepcion Martin <dennisconcepcionmartin@gmail.com>
2021-10-24 15:19:56 +0200

    restart project

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-import os
-from src.puppy.block import read_block
-
-
 def main():
     file_path = '/Users/dennis/Bitcoin/blocks/blk00000.dat'
 
     with open(file_path, 'rb') as f:
-        number_of_bytes_in_file = os.path.getsize(file_path)
-
-        while f.tell() < number_of_bytes_in_file:
-            block = read_block(f)
+        pass
 
 
 if __name__ == '__main__':
